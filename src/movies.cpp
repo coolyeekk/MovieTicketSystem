21901fd05d788568d71d0ee9876b4ebff5f8390c
Shella <78293081+shellaakmad@users.noreply.github.com>
2021-01-31 15:15:43 +0800

    Update movie titles.cpp
    Added 5 Movie Names

--- src/movies.cpp
+++ src/movies.cpp
@@ -15,10 +15,11 @@ void movies::listOfAllMovies()
     int selection;
     system("CLS");
     cout << "\n       The available Movies are: " << endl;
-    cout << "\n         1. ";
-    cout << "\n         2. ";
-    cout << "\n         3. ";
-    cout << "\n         4. ";
+    cout << "\n         1. It takes a man and a woman";
+    cout << "\n         2. Pamilyang Ordinaryo";
+    cout << "\n         3. Alive";
+    cout << "\n         4. Peninsula";
+    cout << "\n         5. Ashfall";
     cout << "" << endl;
     cout << "\n       Enter the movie number to show the movie's info. Otherwise, enter 0 to return to Main Menu.";
     cout << "\n       Type here: "; cin >> selection;
