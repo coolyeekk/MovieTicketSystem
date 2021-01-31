4e14ed22a5ea42b1675d993aed2ce4a903b75f7c
Jonn S <70318724+jonncrs@users.noreply.github.com>
2021-01-31 14:06:29 +0800

    commit initial project files

---
+++ src/tickets.cpp
@@ -0,0 +1,31 @@
+#include "tickets.h"
+
+tickets::tickets()
+{
+    //ctor
+}
+
+tickets::~tickets()
+{
+    //dtor
+}
+
+void tickets::checkTicketStatus()
+{
+
+}
+
+void tickets::editTicketDetails()
+{
+
+}
+
+void tickets::deleteTicket()
+{
+
+}
+
+void tickets::createTicket()
+{
+
+}
